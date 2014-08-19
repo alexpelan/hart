2effb6ae4155baeffc9cf960a58ce9885b82ea49
Alex Pelan <alexpelan@gmail.com>
2014-08-18 17:58:16 -0700

    Improve display of tweets

--- app/assets/javascripts/models/tweet.js
+++ app/assets/javascripts/models/tweet.js
@@ -2,6 +2,7 @@ App.Tweet = DS.Model.extend({
 	text: DS.attr('string'),
 	timestamp: DS.attr('string'),
 	username: DS.attr('string'),
-	
+	tweet_url: DS.attr('string'),
+
 	tweets: DS.belongsTo('tweets'),
 })
--- app/assets/javascripts/models/tweets.js
+++ app/assets/javascripts/models/tweets.js
@@ -14,13 +14,61 @@ App.Tweets = DS.Model.extend({
 			var text = tweets[i].text;
 			var username = tweets[i].user.screen_name;
 			var timestamp = tweets[i].created_at;
-			var new_tweet = this.store.createRecord("tweet", {text: text, username: username, timestamp: timestamp});
+			var tweet_id = tweets[i].id_str;
+			//process/transform data for display
+			//these next two add html, and then we output the unescaped html.
+			text = this.parse_links(text);
+			text = this.parse_hashtags_and_replies(text);
+			var tweet_url = this.make_tweet_url(tweet_id);
+			timestamp = this.parse_twitter_date(timestamp);
+			
+			var new_tweet = this.store.createRecord("tweet", {text: text, username: username, timestamp: timestamp, tweet_url: tweet_url});
 			this.get("tweet_records").addObject(new_tweet);
 			new_tweet.save();
 		}
 
 		this.save();
 
+	},
+
+	//input: <day of week> <month> <day> <24 hr time hh:mm::ss> <timezone?> <year>
+	//output: <day of week> <month> <day> <24 hr time hh:mm>
+	parse_twitter_date: function(timestamp){
+		var date_tokens = timestamp.split(":");
+		return date_tokens[0] + date_tokens[1];
+	},
+
+	parse_links: function(text){
+		var words = text.split(" ");
+		var i;
+		var parsed_text = "";
+		for(i = 0; i < words.length; i++){
+			if( words[i].search("http://") !== -1){
+				words[i] = '<a href="'+ words[i] + '">' + words[i] + '</a>';
+			}
+			parsed_text = parsed_text + words[i] + " ";
+		}
+
+		return parsed_text;
+	},
+
+	parse_hashtags_and_replies: function(text){
+		var words = text.split(" ");
+		var i;
+		var parsed_text = "";
+		for(i = 0; i < words.length; i++){
+			if( words[i][0] === "#" || words[i][0] === "@"){
+				words[i] = '<span class="hashtag_or_reply">' + words[i] + '</span>';
+			}
+
+			parsed_text = parsed_text + words[i] + " ";
+		}
+
+		return parsed_text;
+	},
+
+	make_tweet_url: function(tweet_id){
+		return "http://twitter.com/alexpelan/status/" + tweet_id;
 	},
 	
 	link_to_command: function(command){
